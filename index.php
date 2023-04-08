0cac479017f58a1b8816fe04200a8409235c44a8
vladxdd <vladcorcodel12@mail.ru>
2023-04-08 19:26:18 +0300

    fix insta face link

--- index.php
+++ index.php
@@ -54,17 +54,22 @@
         <li><a class="nav-link" href="#resume">Resume</a></li>
         <li><a class="nav-link" href="#services">Services</a></li>
         <li><a class="nav-link" href="#portfolio">Portfolio</a></li>
-        <li><a class="nav-link" href="">Puncte: 0</a></li>
+        <li><a class="nav-link" href="#score">Puncte: 0</a></li>
         <li><a class="nav-link" href="#contact">Contact</a></li>
       </ul>
       <i class="bi bi-list mobile-nav-toggle"></i>
     </nav><!-- .navbar -->
 
     <div class="social-links">
-      <a href="#" class="twitter"><i class="bi bi-twitter"></i></a>
-      <a href="#" class="facebook"><i class="bi bi-facebook"></i></a>
-      <a href="#" class="instagram"><i class="bi bi-instagram"></i></a>
-      <a href="#" class="linkedin"><i class="bi bi-linkedin"></i></a>
+      <a href="https://www.facebook.com/europalibera.org/?locale=ru_RU" class="facebook" target="_blank">
+        <i class="bi bi-facebook"></i>
+      </a>
+      <a href="https://www.instagram.com/_.vlad.c._/" class="instagram" target="_blank">
+        <i class="bi bi-instagram"></i>
+      </a>
+      <a href="https://www.linkedin.com/in/vlad-corcodel-461a2a250/" class="instagram" target="_blank">
+        <i class="bi bi-linkedin"></i>
+      </a>
     </div>
 
   </div>
@@ -100,6 +105,13 @@
   require_once "pages/portfolio.php";
   ?>
 </section><!-- End Portfolio Section -->
+
+<!-- ======= Score ======= -->
+<section id="score" class="score">
+  <?php
+  require_once "pages/score.php";
+  ?>
+</section><!-- Score -->
 
 <!-- ======= Contact Section ======= -->
 <section id="contact" class="contact">
