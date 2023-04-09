b4fdb89f0b9ebd29c6689b7e09b3cae11f5faf38
CorneliuCB <cebotarenco.corneliu07@gmail.com>
2023-04-09 15:19:40 +0300

    fdfdf

---
+++ pages/score.php
@@ -0,0 +1,109 @@
+<div class="about-me container">
+
+  <div class="section-title">
+    <h2>Info</h2>
+    <p class="titluinfo">Află mai multe despre candidați</p>
+  </div>
+
+  <div class="par">
+<p>
+  Aici puteți găsi toată informația necesară despre toți candidații la alegerile locale din Republica Moldova.
+</p>
+<p class="tt">
+  Trebuie doar să introguceți locația.
+</p>
+</div>
+<div class="card">
+    <span class="card__title">Introduceți aici</span>
+    <p class="card__content">Puteți selecta orice locație
+    </p>
+    <div class="card__form">
+        <input placeholder="Locația dvs." type="text">
+        <button class="sign-up"> Căutare</button>
+    </div>
+</div>
+  
+<!-- ======= Testimonials ======= -->
+<div class="testimonials container">
+
+  <div class="section-title">
+    <h2>Informație adăugată de utilizatori</h2>
+  </div>
+
+  <div class="testimonials-slider swiper" data-aos="fade-up" data-aos-delay="100">
+    <div class="swiper-wrapper">
+
+      <div class="swiper-slide">
+        <div class="testimonial-item">
+          <p>
+            <i class="bx bxs-quote-alt-left quote-icon-left"></i>
+            Sunt voluntar la secția de votare observ că unii dintre reprezentanții partidelor politice încearcă să se apropie prea mult de cabinele de vot sau încearcă să influențeze alegătorii. Eu intervin și îi informez pe aceștia că acest comportament este ilegal și că trebuie să respecte regulile procesului electoral. Ei își cer scuze și își modifică comportamentul pentru a nu influența procesul electoral. 
+            <i class="bx bxs-quote-alt-right quote-icon-right"></i>
+          </p>
+          <img src="assets/img/testimonials/photo1.jpg" class="testimonial-img" alt="">
+          <h3>Drumea Nicu</h3>
+          <h4>Student</h4>
+        </div>
+      </div><!-- End testimonial item -->
+
+      <div class="swiper-slide">
+        <div class="testimonial-item">
+          <p>
+            <i class="bx bxs-quote-alt-left quote-icon-left"></i>
+            Am observant că unele afișe ale partidelor politice nu au fost amplasate în mod egal în zonele publice și că au fost unele nereguli în distribuirea materialelor electorale. Am trimis sesizări către autoritățile electorale și cer să se ia măsuri pentru a se asigura o competiție corectă. 
+            <i class="bx bxs-quote-alt-right quote-icon-right"></i>
+          </p>
+          <img src="assets/img/testimonials/photo2.jpg" class="testimonial-img" alt="">
+          <h3>Camelia Tuzău</h3>
+          <h4>Studentă</h4>
+        </div>
+      </div><!-- End testimonial item -->
+
+      <div class="swiper-slide">
+        <div class="testimonial-item">
+          <p>
+            <i class="bx bxs-quote-alt-left quote-icon-left"></i>
+            Am   participat pentru prima dată la alegeri am observat că unele secții de votare au liste de alegători incomplete sau că unele cabine de votare nu au fost suficient de bine protejate pentru a asigura confidențialitatea votului. Am luat legături cu organizațiile civice și am informat despre aceste probleme pentru a putea fi remediate în timpul procesului electoral. 
+            <i class="bx bxs-quote-alt-right quote-icon-right"></i>
+          </p>
+          <img src="assets/img/testimonials/photo3.jpg" class="testimonial-img" alt="">
+          <h3>Victoria Bunacalea</h3>
+          <h4>Designer</h4>
+        </div>
+      </div><!-- End testimonial item -->
+
+      <div class="swiper-slide">
+        <div class="testimonial-item">
+          <p>
+            <i class="bx bxs-quote-alt-left quote-icon-left"></i>
+            În procesul de votare,am descoperit că numele meu nu apare pe lista de alegători. Eu mi-am verificat actele și m-am asigurat că am îndeplinit toate condițiile pentru a vota,fără ca să cunosc am aflat că votul meu a fost predecis. 
+            <i class="bx bxs-quote-alt-right quote-icon-right"></i>
+          </p>
+          <img src="assets/img/testimonials/photo4.jpg" class="testimonial-img" alt="">
+          <h3>Ion Sandu</h3>
+          <h4>Data analist</h4>
+        </div>
+      </div><!-- End testimonial item -->
+
+      <div class="swiper-slide">
+        <div class="testimonial-item">
+          <p>
+            <i class="bx bxs-quote-alt-left quote-icon-left"></i>
+            Am descoperit că unii dintre observatori ai partidului guvernamental încercau să intimideze alegătorii și să le influențeze votul. Informația deplină cu proof-uri a fost trimisă pe poștă.
+            <i class="bx bxs-quote-alt-right quote-icon-right"></i>
+          </p>
+          <img src="assets/img/testimonials/testimonials-5.jpg" class="testimonial-img" alt="">
+          <h3>Daniel Cașu</h3>
+          <h4>Antreprenor</h4>
+        </div>
+      </div><!-- End testimonial item -->
+
+    </div>
+    <div class="swiper-pagination"></div>
+  </div>
+
+  <div class="owl-carousel testimonials-carousel">
+
+  </div>
+
+</div><!-- End Testimonials  -->
