d80a3b6f979bdb8c18564b5d788f03488f0c1746
xsx4334 <xsx433@gmail.com>
2023-04-09 10:08:58 +0300

    add bot in services

--- pages/services.php
+++ pages/services.php
@@ -1,59 +1,45 @@
-<div class="container">
+<!DOCTYPE html>
+<html>
 
-  <div class="section-title">
-    <h2>Services</h2>
-    <p>My Services</p>
-  </div>
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
+    <link rel="shortcut icon"
+        href="https://tars-file-upload.s3.amazonaws.com/qhnqJc/b8862d91c6aa1ef77a0648dc9276afc9--principiile-protectiei-datelor-cu-caracter-personal.jpg"
+        type="image/x-icon" />
+    <meta property="og:image"
+        content="https://tars-file-upload.s3.amazonaws.com/qhnqJc/b8862d91c6aa1ef77a0648dc9276afc9--principiile-protectiei-datelor-cu-caracter-personal.jpg" />
+    <title>Colectare de Informatii</title>
+    <style type="text/css">
+        body,
+        html {
+            margin: 0;
+            padding: 0;
+            height: 100%;
+            width: 100%;
+        }
 
-  <div class="row">
-    <div class="col-lg-4 col-md-6 d-flex align-items-stretch">
-      <div class="icon-box">
-        <div class="icon"><i class="bx bxl-dribbble"></i></div>
-        <h4><a href="">Lorem Ipsum</a></h4>
-        <p>Voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi</p>
-      </div>
+        .tars-fullpage-container-class {
+            position: fixed;
+            top: 0;
+            left: 0;
+            right: 0;
+            bottom: 0;
+            border: 0;
+        }
+
+        #tars-fullpage {
+            width: 100%;
+            height: 100%;
+        }
+    </style>
+</head>
+
+<body>
+    <div id="tars-fullpage-container" class="tars-fullpage-container-class">
+        <iframe id="tars-fullpage" frameborder="0" allow="geolocation; microphone; camera; midi; encrypted-media;"
+            src="https://Colectare_Informatii.hellotars.com/conv/qhnqJc/"></iframe>
+        <script type="text/javascript" src="https://tars-file-upload.s3.amazonaws.com/bulb/js/fullpage.js"></script>
     </div>
+</body>
 
-    <div class="col-lg-4 col-md-6 d-flex align-items-stretch mt-4 mt-md-0">
-      <div class="icon-box">
-        <div class="icon"><i class="bx bx-file"></i></div>
-        <h4><a href="">Sed ut perspiciatis</a></h4>
-        <p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore</p>
-      </div>
-    </div>
-
-    <div class="col-lg-4 col-md-6 d-flex align-items-stretch mt-4 mt-lg-0">
-      <div class="icon-box">
-        <div class="icon"><i class="bx bx-tachometer"></i></div>
-        <h4><a href="">Magni Dolores</a></h4>
-        <p>Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia</p>
-      </div>
-    </div>
-
-    <div class="col-lg-4 col-md-6 d-flex align-items-stretch mt-4">
-      <div class="icon-box">
-        <div class="icon"><i class="bx bx-world"></i></div>
-        <h4><a href="">Nemo Enim</a></h4>
-        <p>At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis</p>
-      </div>
-    </div>
-
-    <div class="col-lg-4 col-md-6 d-flex align-items-stretch mt-4">
-      <div class="icon-box">
-        <div class="icon"><i class="bx bx-slideshow"></i></div>
-        <h4><a href="">Dele cardo</a></h4>
-        <p>Quis consequatur saepe eligendi voluptatem consequatur dolor consequuntur</p>
-      </div>
-    </div>
-
-    <div class="col-lg-4 col-md-6 d-flex align-items-stretch mt-4">
-      <div class="icon-box">
-        <div class="icon"><i class="bx bx-arch"></i></div>
-        <h4><a href="">Divera don</a></h4>
-        <p>Modi nostrum vel laborum. Porro fugit error sit minus sapiente sit aspernatur</p>
-      </div>
-    </div>
-
-  </div>
-
-</div>
+</html>
